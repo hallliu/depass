6f3579211c1ee06eb9d6e559c1b4d530e08d3de9
Hall Liu <hallliu000@gmail.com>
2015-07-05 17:10:01 -0500

    prettified up the dialog box, though it still looks ugly, and added a modifier field

--- data/settings.js
+++ data/settings.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
     addon.port.on("pwCheckResult", function(valid) {
-        $("#isValid").text(valid ? "Valid" : "Invalid");
+        $("#feedbackPlace").removeClass("has-error has-success");
+        $("#feedbackPlace").addClass(valid ? "has-success" : "has-error");
+        $("#masterPwIcon").removeClass("glyphicon-ok glyphicon-remove");
+        $("#masterPwIcon").addClass(valid ? "glyphicon-ok" : "glyphicon-remove");
     });
 
     addon.port.on("setOptionsDisplay", function(options) {
         for (optName in options) {
             if (options.hasOwnProperty(optName)) {
-                $('#' + optName)[0].checked = options[optName];
+                if (optName == "modifiers") {
+                    $('#' + optName).val(options[optName]);
+                } else {
+                    $('#' + optName)[0].checked = options[optName];
+                }
             }
         }
     });
@@ -26,7 +33,11 @@ $(document).ready(function() {
 
     $(".pwOption").change(function(event) {
         options = {}
-        options[event.target.id] = event.target.checked;
+        if (event.target.id === "modifiers") {
+            options[event.target.id] = event.target.value;
+        } else {
+            options[event.target.id] = event.target.checked;
+        }
         addon.port.emit("optionsChange", options);
     });
 });
