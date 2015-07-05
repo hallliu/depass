96211d21169ac105f30c4bc1067538cfb1130bc9
Hall Liu <hallliu000@gmail.com>
2015-07-04 23:06:44 -0500

    start adding in loading stuff from storage

--- data/settings.js
+++ data/settings.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
         $("#isValid").value = valid ? "Valid" : "Invalid";
     });
 
+    addon.port.on("setOptionsDisplay", function(options) {
+        // Set checkboxes to options given
+    });
+
     $("#masterPassword").keypress(function(event) {
         var enteredPw = $(event.target).val();
         addon.port.emit("checkThisPw", enteredPw);
@@ -13,6 +17,4 @@ $(document).ready(function() {
         options[event.target.id] = event.target.checked;
         addon.port.emit("optionsChange", options);
     });
-
-
 });
